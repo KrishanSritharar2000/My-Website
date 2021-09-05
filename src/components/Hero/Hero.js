d73799b5d79a35919fee16d243464bd1fc6385ef
Krishan Sritharar <ks1619@ic.ac.uk>
2021-09-05 13:20:53 +0100

    Title and projects

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,19 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+      I'm an enthusiastic Second Year computer scientist, eagerly looking to improve and broaden my skillset. 
+      </SectionText>
+
+      <Button onClick={() => window.location = 'https://www.google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
