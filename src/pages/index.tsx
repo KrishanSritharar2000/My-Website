654ee086e47bb9601ae8fa8e19eb244ae062bbb4
Krishan Sritharar <ks1619@ic.ac.uk>
2021-09-06 14:34:39 +0100

    Added repo information as constant data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
-import axios from 'axios';
-import { GetStaticProps } from 'next';
-import { RepositoryEdge } from 'generated/graphql';
 import Layout from 'components/ui/Layout';
 import SEO from 'components/SEO';
 import Intro from 'components/modules/Intro';
 import Projects from 'components/modules/Projects';
 import Skills from 'components/modules/Skills';
 import Contact from 'components/modules/Contact';
+import { repos } from 'constants/constants';
 
-const HomePage = ({ repos }: { repos: RepositoryEdge[] }) => (
+const HomePage = () => (
   <Layout>
     <SEO />
     <Intro />
@@ -18,60 +16,13 @@ const HomePage = ({ repos }: { repos: RepositoryEdge[] }) => (
   </Layout>
 );
 
-export const getStaticProps: GetStaticProps = async () => {
-  const {
-    data: { data },
-  } = await axios({
-    url: 'https://api.github.com/graphql',
-    method: 'post',
-    data: {
-      query: `
-				query viewer {
-					viewer {
-						repositories(first: 9, orderBy: {field: CREATED_AT, direction: DESC}) {
-							edges {
-								node {
-									id
-									name
-									url
-									description
-									stargazers {
-										totalCount
-									}
-									forkCount
-									languages(first: 3) {
-										nodes {
-											id
-											name
-										}
-									}
-									defaultBranchRef {
-										target {
-											... on Commit {
-												history {
-													totalCount
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			`,
-    },
-    headers: {
-      Authorization: `bearer `,
-    },
-  });
-
-  return {
-    props: {
-      repos: data.viewer.repositories.edges,
-    },
-    revalidate: 10,
-  };
+export type Respository = {
+  name: string;
+  description: string;
+  commits: number;
+  branches: number;
+  languages: string[];
+  url: string;
 };
 
 export default HomePage;
